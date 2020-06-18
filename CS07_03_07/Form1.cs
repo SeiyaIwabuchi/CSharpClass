7370209bed38b0931e6e0fba724e26974470ff69
SeiyaIwabuchi <0123ook@gmail.com>
2020-06-18 13:36:25 +0900

    初期コミット

---
+++ CS07_03_07/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS07_03_07
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton tmp = (RadioButton)sender;
+            label1.Text = tmp.Text + "を選びました。";
+        }
+    }
+}
---
+++ CS07_07_15/Record.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS07_07_15
+{
+    class Record
+    {
+        public string name;
+        public double attend;
+        public int grade;
+        public Record(string n,float a,int g)
+        {
+            name = n;
+            attend = a;
+            grade = g;
+        }
+    }
+}
