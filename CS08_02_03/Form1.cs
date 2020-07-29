3a742cd4333b5eec39bd9fb069d6904af47e2919
SeiyaIwabuchi <0123ook@gmail.com>
2020-07-29 12:49:49 +0900

    画像の色が変わらない。。。

---
+++ CS08_02_03/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS08_02_03
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap bm1, bm2;
+        private int i;
+        //ColorMatrixオブジェクトの作成
+        //グレースケールに変換するための行列を指定する
+        private System.Drawing.Imaging.ColorMatrix cm =
+            new System.Drawing.Imaging.ColorMatrix(
+                new float[][]{
+                    new float[]{0.299f, 0.299f, 0.299f, 0 ,0},
+                    new float[]{0.587f, 0.587f, 0.587f, 0, 0},
+                    new float[]{0.114f, 0.114f, 0.114f, 0, 0},
+                    new float[]{0, 0, 0, 1, 0},
+                    new float[]{0, 0, 0, 0, 1}
+                });
+        //ImageAttributesオブジェクトの作成
+        private System.Drawing.Imaging.ImageAttributes ia =
+        new System.Drawing.Imaging.ImageAttributes();
+
+        //bm1のGraphicsオブジェクトを取得
+        private Graphics g;
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            g.DrawImage(bm2,new Rectangle(0,0,bm1.Width,bm1.Height), 0, 0, bm1.Width, bm1.Height, GraphicsUnit.Pixel, ia);
+            pictureBox1.Image = bm2;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            bm1 = new Bitmap("../../tea.jpg");
+            bm2 = new Bitmap("../../tea.jpg");
+            pictureBox1.Image = bm1;
+            ia.SetColorMatrix(cm);
+            //bm1のGraphicsオブジェクトを取得
+            g = Graphics.FromImage(bm2);
+    }
+        
+    }
+}
---
+++ CS08_02_03_01/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS08_02_03_01
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap bm1, bm2;
+        //ColorMatrixオブジェクトの作成
+        //グレースケールに変換するための行列を指定する
+        private System.Drawing.Imaging.ColorMatrix cm;
+
+        private int i=0;
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Bitmap newImg = new Bitmap(bm1.Width, bm1.Height);
+            Graphics g = Graphics.FromImage(newImg);
+            float[][] cma = new float[][]{
+                    new float[]{0.299f, 0.299f, 0.299f, 0 ,0},
+                new float[]{0.587f, 0.587f, 0.587f, 0, 0},
+                new float[]{0.114f, 0.114f, 0.114f, 0, 0},
+                new float[]{0, 0, 0, 1, 0},
+                new float[]{0, 0, 0, 0, 1}
+                };
+            foreach (float[] t in cma){
+                Console.Write("[");
+                foreach (float v in t) Console.Write(v + ",");
+                Console.WriteLine("]");
+            }
+            Console.WriteLine();
+            cm = new System.Drawing.Imaging.ColorMatrix(cma);
+            i = ++i % 6;
+            //ImageAttributesオブジェクトの作成
+            System.Drawing.Imaging.ImageAttributes ia = new System.Drawing.Imaging.ImageAttributes();
+            g.DrawImage(bm1, new Rectangle(0, 0, bm1.Width, bm1.Height), 0, 0, bm1.Width, bm1.Height, GraphicsUnit.Pixel, ia);
+            ia.SetColorMatrix(cm);
+            pictureBox1.Image = newImg;
+            //g.Dispose();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            bm1 = new Bitmap("../../tea.jpg");
+            bm2 = new Bitmap("../../tea.jpg");
+            pictureBox1.Image = bm1;
+            //bm1のGraphicsオブジェクトを取得
+        }
+
+    }
+}
