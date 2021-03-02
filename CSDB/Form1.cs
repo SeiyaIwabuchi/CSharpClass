8f68093c8c3a37d664e4e85cd97aeec9fd0a5547
岩渕 誠也 <180453@jc-21.jp>
2021-03-02 13:35:16 +0900

    update

--- CSDB/Form1.cs
+++ CSDB/Form1.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Data.OleDb;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -13,52 +11,48 @@ namespace CSDB
 {
     public partial class Form1 : Form
     {
-        string strAccessConn = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=CSDB.accdb";
-        string strAccessSelect = "SELECT * FROM gakusei";
-
+        Gakusei gakusei = new Gakusei();
         public Form1()
         {
             InitializeComponent();
-            OleDbConnection myAccessConn = null;
+            textBox1.Text = "100";
+            label1.Text = "";
             try
             {
-                myAccessConn = new OleDbConnection(strAccessConn);
-            }
-            catch (Exception ex)
-            {
-                label1.Text = "Error: Failed connection. " + ex.Message;
-                return;
-            }
-            try
-            {
-                OleDbCommand myAccessCommand
-               = new OleDbCommand(strAccessSelect, myAccessConn);
-                myAccessConn.Open();
-                OleDbDataReader reader = myAccessCommand.ExecuteReader();
-                while (reader.Read())
+                foreach (Gakusei g in gakusei.getAll())
                 {
-                    listBox1.Items.Add(
-                        reader.GetString(1) +
-                        reader.GetString(2) +
-                        reader.GetString(3) +
-                        "  " +
-                        reader.GetString(4) +
-                        "  " +
-                        reader.GetString(5) + 
-                        "%"
-                        );
+                    if (g.Gshusseki > int.Parse(textBox1.Text)) continue;
+                    listBox1.Items.Add(String.Format("{0}{1}{2}\t{3}\t{4} %", g.Gakkacode, g.Gakunen, g.Gclass, g.Gname, g.Gshusseki));
                 }
             }
             catch (Exception ex)
             {
-                label1.Text = "Error: Failed from the DataBase. "+ ex.Message;
+                label1.Text = "Error: Failed from the DataBase. " + ex.Message;
                 return;
             }
-            finally
-            {
-                myAccessConn.Close();
-            }
+        }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            try
+            {
+                foreach (Gakusei g in gakusei.getAll())
+                {
+                    if (g.Gshusseki > int.Parse(textBox1.Text)) continue;
+                    listBox1.Items.Add(String.Format("{0}{1}{2}\t{3}\t{4} %", g.Gakkacode, g.Gakunen, g.Gclass, g.Gname, g.Gshusseki));
+                }
+            }
+            catch (FormatException fe)
+            {
+                label1.Text = fe.Message;
+                return;
+            }
+            catch (Exception ex)
+            {
+                label1.Text = "Error: Failed from the DataBase. " + ex.Message;
+                return;
+            }
         }
     }
 }
